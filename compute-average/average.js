28c620e6ea8b91801c1b06c2840bd47041f24d6b
Aaron Whitehead <whitehead.aaron@live.com>
2018-08-21 09:22:30 -0700

    added average js

---
+++ compute-average/average.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const myArray = [1, 2, 3, 4, 5, 6];
+
+function average(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+
+console.log(average(myArray));
