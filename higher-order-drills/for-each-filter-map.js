6e78c5f40455308f2bfa34c295230495b9226e68
Aaron Whitehead <whitehead.aaron@live.com>
2018-08-21 12:44:57 -0700

    forEach filter and map

---
+++ higher-order-drills/for-each-filter-map.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// Filter method (although I didn't do it right)
+
+const myArray = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
+let newArray = [];
+
+for (let i = 0; i < myArray.length; i++) {
+  if (myArray[i][0] >= 0 && myArray[i][1] >= 0) {
+    newArray.push(myArray[i]);
+  }
+}
+
+const filtered = newArray.filter(function (item) {
+
+  return item;
+});
+console.log(filtered);
+
+
+
+
+
+// Map method (yay, I did it!)
+
+
+const mapArray = myArray.map(num => num[0] + num[1]);
+
+console.log(mapArray);
+
+
+
+// forEach method
+
+
+const ohHey = myArray.forEach((item) => console.log((Math.abs(item[0]) + Math.abs(item[1]))));
+
+
+
+
--- higher-order-drills/return-values.js
+++ higher-order-drills/return-values.js
@@ -1,15 +1,15 @@
 'use strict';
 
 function hazardWarningCreator(typeOfWarning) {
-    let warningCounter = 0;
+  let warningCounter = 0;
 
-    
-    return function(location) {
-        warningCounter += 1;
-        console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
-        console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`)
-        return warningCounter;
-    }
+
+  return function (location) {
+    warningCounter += 1;
+    console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+    console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`)
+    return warningCounter;
+  };
 }
 
 const rocksWarning = hazardWarningCreator('Rocks on the Road');
