3572903433d930bd9f8aaac368f395c6d8983638
Aaron Whitehead <whitehead.aaron@live.com>
2018-08-21 18:54:45 -0700

    added some explanations

--- higher-order-drills/reduce.js
+++ higher-order-drills/reduce.js
@@ -32,15 +32,16 @@ console.log(reduceIt(arr));
 
 const input = 'noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest';
 const decoded = input
-  .split(' ')
-  .reduce((decodedString, word) => {
-    if (word.length === 3) return decodedString + ' ';
-
-    return decodedString + word[word.length - 1].toUpperCase();
-  }, '');
+  .split(' ')  // This is splitting the string 'input' into an array
+  .reduce((decodedString, word) => {     //  This is setting up the reduce function. It will run for as long as there is a word
+    if (word.length === 3) return decodedString + ' ';  // decodedString is like a running count of what we have so far
+    // decodedString stores what he have. So when you return decodedString + a space, it just adds a space to what was there
+    return decodedString + word[word.length - 1].toUpperCase();  //  this is adding the last letter of the words to our string
+  }, '');  //  You need the '' so that the string is empty to begin with. It will break without this
 console.log(decoded);
 
 
+// This is the same things as above, just longer notation
 const hello = 'noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest';
 const solution = hello.split(' ');
 
