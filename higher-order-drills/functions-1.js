e16120705cce2def1ff3e39b3615c57a0f5b59e9
Aaron Whitehead <whitehead.aaron@live.com>
2018-08-21 19:16:44 -0700

    final changes for now

--- higher-order-drills/functions-1.js
+++ higher-order-drills/functions-1.js
@@ -1,17 +1,17 @@
 'use strict';
 
 function repeat(fn, n) {
-    for (let i = 0; i < n; i++) {
-        fn();
-    }
+  for (let i = 0; i < n; i++) {
+    fn();  // This is where we were stuck when we started this problem, this is the correct way to call the function
+  }
 }
 
 function hello() {
-    console.log('Hello world');
+  console.log('Hello world');
 }
 
 function goodbye() {
-    console.log('Goodbye world');
+  console.log('Goodbye world');
 }
 
 repeat(hello, 5);
--- higher-order-drills/functions-2.js
+++ higher-order-drills/functions-2.js
@@ -1,16 +1,15 @@
 'use strict';
 
 function filter(arr, fn) {
-    // TASK: Define your function here
-    let newArray = [];
-
-    for (let i = 0; i < arr.length; i++) {
-
-        if (arr[i][0] === 'R') {
-            newArray.push(arr[i]);
-        }
+  // TASK: Define your function here
+  // This isn't exactly the way they wanted us to do this, but I ran out of time and it works so I left it
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i][0] === 'R') {
+      newArray.push(arr[i]);
     }
-    return newArray;
+  }
+  return newArray;
 }
 
 // DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
@@ -20,10 +19,10 @@ const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 // We use your `filter` function here, capturing a new array into `filteredNames`
 // comprised of names that only begin with 'R'
 const filteredNames = filter(myNames, function (name) {
-    // This is known as a "predicate function" - it's a function that 
-    // only returns a boolean
-    return name[0] === 'R';
+  // This is known as a "predicate function" - it's a function that 
+  // only returns a boolean
+  return name[0] === 'R';
 });
 
-console.log(filteredNames) // => ['Rich', 'Ray']
+console.log(filteredNames);  // => ['Rich', 'Ray']
 // <---- DO NOT EDIT BETWEEN THESE LINES
